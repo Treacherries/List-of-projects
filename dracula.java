b71ce2a913d01db56465c798a37afd620aab2b84
Treacherries <mychococloud@gmail.com>
2024-11-24 18:48:47 +0800

    Create dracula.java
    project on cc103

---
+++ dracula.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class dracula {
+    public static void main(String[] args) {
+        double std = 1800.00;
+        double dxl = 2300.00;
+        double sui = 3000.00;
+    }   
+}
